15e0c570330116ba2b31b128e99abd820bb0e54b
LADRAT Mattéo <apokalyptdev@gmail.com>
2021-03-07 20:58:19 +0100

    -- Add security on user route -- Add route to generate access token

--- back_end/routes/UserRoute.js
+++ back_end/routes/UserRoute.js
@@ -4,11 +4,12 @@ const DataObject = require("../models/DataObject");
 const { login } = require("../methods/User");
 const jwt = require("jsonwebtoken");
 const { retrieveUserData } = require("../methods/User");
-const { authenticateModule } = require("../security/authenticate");
+const { authenticateModule } = require("../security/middleware");
 const { retrieveAllUserNear } = require("../methods/Location");
 
 module.exports.includeAllUserRoutes = (app) => {
     loginRoute(app);
+    generateAccessTokenRoute(app);
 
     retrieveUserDataRoute(app);
     retrieveAllUserNearRoute(app);
@@ -37,7 +38,7 @@ const retrieveAllUserNearRoute = (app) => {
                 Promise.all(data).then((users) => {
                     const responseData = new ResponseData("Successful recovery of users close to you !", ResponseCode.S_Success, users);
                     res.status(200).json(responseData)
-                }).catch((error) => {
+                }).catch(() => {
                     const responseData = new ResponseData("An unknown error occurred !", ResponseCode.E_UnknownError, new DataObject());
                     res.status(500).json(responseData)
                 })
@@ -49,8 +50,19 @@ const retrieveAllUserNearRoute = (app) => {
     });
 }
 
+const generateAccessTokenRoute = (app) => {
+    app.get("/api/:version/user/:id/access", authenticateModule, (req, res) => {
+        const data = {
+            token : jwt.sign({ID: req.params.id}, process.env.SECURITY_KEY, { expiresIn: "1m"})
+        }
+
+        const responseData = new ResponseData("Token access generated !", ResponseCode.S_Success, data);
+        res.status(200).json(responseData)
+    });
+}
+
 const retrieveUserDataRoute = (app) => {
-    app.get("/api/:version/user/:id", (req, res) => {
+    app.get("/api/:version/user/:id", authenticateModule, (req, res) => {
         retrieveUserData(req.params.id).then((data) => {
             if (!!data) {
                 const responseData = new ResponseData("Successful recovery of user data !", ResponseCode.S_Success, data);
---
+++ back_end/security/middleware.js
@@ -0,0 +1,33 @@
+const ResponseData = require("../models/Response/ResponseData");
+const ResponseCode = require("../models/Response/ResponseCode");
+const DataObject = require("../models/DataObject");
+
+const jwt = require("jsonwebtoken");
+
+module.exports = {
+    authenticateModule(req, res, next) {
+        const authorizationHeader = req.headers.authorization;
+
+        if (!authorizationHeader) {
+            const responseData = new ResponseData("Authentication token not included !", ResponseCode.E_NoToken, new DataObject());
+            return res.status(400).json(responseData);
+        } else {
+            const token = authorizationHeader.split(" ")[1];
+            jwt.verify(token, process.env.SECURITY_KEY, (error, decodedToken) => {
+                if (error) {
+                    const responseData = new ResponseData("You are not authorized to make this request !", ResponseCode.E_Unauthorized, new DataObject());
+                    return res.status(401).json(responseData);
+                } else {
+                    const userID = decodedToken.ID;
+
+                    if (req.params.ID && req.params.ID !== userID) {
+                        const responseData = new ResponseData("Token invalid !", ResponseCode.E_Unauthorized, new DataObject());
+                        return res.status(401).json(responseData);
+                    } else {
+                        return next();
+                    }
+                }
+            });
+        }
+    }
+}
