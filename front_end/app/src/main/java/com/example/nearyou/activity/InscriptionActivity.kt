fd0b4d8a5274eee666e27e2f0befb00356608b40
Emma-NVG <51115377+Emma-NVG@users.noreply.github.com>
2021-03-31 23:09:41 +0200

    -- adding inscription activity

---
+++ front_end/app/src/main/java/com/example/nearyou/activity/InscriptionActivity.kt
@@ -0,0 +1,12 @@
+package com.example.nearyou.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.nearyou.R
+
+class InscriptionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inscription)
+    }
+}
