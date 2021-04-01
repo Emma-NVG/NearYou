980291786e743c1fffc0ae779a2eb93d3427740e
LADRAT Mattéo <apokalyptdev@gmail.com>
2021-04-01 21:25:36 +0100

    -- Patch error on ResponseCode

--- front_end/app/src/main/java/com/example/nearyou/model/response/ResponseCode.kt
+++ front_end/app/src/main/java/com/example/nearyou/model/response/ResponseCode.kt
@@ -1,6 +1,7 @@
 package com.example.nearyou.model.response
 
 enum class ResponseCode(code: String) {
+    NO_ERROR_KNOWN(""),
     E_UNKNOWN_ROUTE("E-UnknownRoute"),
     E_UNKNOWN_ERROR("E-UnknownError"),
     E_NO_TOKEN("E-NoToken"),
@@ -15,5 +16,30 @@ enum class ResponseCode(code: String) {
     E_PASSWORD_TOO_SHORT("E-PasswordTooShort"),
     E_SURNAME_TOO_LONG("E-SurnameTooLong"),
     E_FIRST_NAME_TOO_LONG("E-FirstNameTooLong"),
-    S_SUCCESS("S-Success")
+    S_SUCCESS("S-Success"),
+}
+
+class ResponseCodeBuilder {
+    companion object {
+        fun getResponseCodeInstance(code: String): ResponseCode {
+            return when (code) {
+                "E-UnknownRoute" -> ResponseCode.E_UNKNOWN_ROUTE
+                "E-UnknownError" -> ResponseCode.E_UNKNOWN_ERROR
+                "E-NoToken" -> ResponseCode.E_NO_TOKEN
+                "E-Unauthorized" -> ResponseCode.E_UNAUTHORIZED
+                "E-NoResource" -> ResponseCode.E_NO_RESOURCE
+                "E-AgeTooYoung" -> ResponseCode.E_AGE_TOO_YOUNG
+                "E-BadEmailFormat" -> ResponseCode.E_BAD_EMAIL_FORMAT
+                "E-EmailKnown" -> ResponseCode.E_EMAIL_KNOWN
+                "E-DatabaseDataTooLong" -> ResponseCode.E_DATABASE_DATA_TOO_LONG
+                "E-EmailTooLong" -> ResponseCode.E_EMAIL_TOO_LONG
+                "E-PasswordTooLong" -> ResponseCode.E_PASSWORD_TOO_LONG
+                "E-PasswordTooShort" -> ResponseCode.E_PASSWORD_TOO_SHORT
+                "E-SurnameTooLong" -> ResponseCode.E_SURNAME_TOO_LONG
+                "E-FirstNameTooLong" -> ResponseCode.E_FIRST_NAME_TOO_LONG
+                "S-Success" -> ResponseCode.S_SUCCESS
+                else -> ResponseCode.NO_ERROR_KNOWN
+            }
+        }
+    }
 }
