31ca48af90d108dd96e67081a9d3555a9fc30b80
Emma-NVG <51115377+Emma-NVG@users.noreply.github.com>
2021-04-03 00:53:25 +0200

    -- correction bug Matteo

--- front_end/app/src/main/java/com/example/nearyou/model/Link.kt
+++ front_end/app/src/main/java/com/example/nearyou/model/Link.kt
@@ -3,8 +3,10 @@ package com.example.nearyou.model
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class Link(private val ID: Int,
-                private val link: String,
-                private val created_date: String,
-                private val edited_date: String) {
+data class Link(
+    private val ID: Int,
+    val link: String,
+    private val created_date: String,
+    private val edited_date: String
+) {
 }
