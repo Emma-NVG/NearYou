6e8139108fa2cc6bede3f221563fcf05bde9ec0b
LADRAT Mattéo <apokalyptdev@gmail.com>
2021-04-02 19:52:40 +0100

    -- Add QR Code generation

--- front_end/app/src/main/java/com/example/nearyou/activity/fragment/profile/access/ProfileAccessFragment.kt
+++ front_end/app/src/main/java/com/example/nearyou/activity/fragment/profile/access/ProfileAccessFragment.kt
@@ -1,11 +1,17 @@
 package com.example.nearyou.activity.fragment.profile.access
 
+import android.graphics.Bitmap
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.nearyou.databinding.FragmentProfileAccessBinding
+import com.example.nearyou.model.user.UserDAO
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.MultiFormatWriter
+import com.google.zxing.WriterException
 
 class ProfileAccessFragment : Fragment() {
 
@@ -21,13 +27,38 @@ class ProfileAccessFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View {
         _binding = FragmentProfileAccessBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        return root
+        return binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        if (UserDAO.user != null) {
+            val bitmap = generateQRCode(UserDAO.user!!.token)
+            binding.qrCode.setImageBitmap(bitmap)
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+
+    private fun generateQRCode(text: String): Bitmap {
+        val width = 800
+        val height = 800
+        val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        val codeWriter = MultiFormatWriter()
+        try {
+            val bitMatrix = codeWriter.encode(text, BarcodeFormat.QR_CODE, width, height)
+            for (x in 0 until width) {
+                for (y in 0 until height) {
+                    bitmap.setPixel(x, y, if (bitMatrix[x, y]) Color.BLACK else Color.WHITE)
+                }
+            }
+        } catch (e: WriterException) { }
+        return bitmap
     }
 }
