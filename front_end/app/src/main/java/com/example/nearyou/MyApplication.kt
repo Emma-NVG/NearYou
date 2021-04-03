48a34dc1306eb5be1b9da16b7afc427b7e825d9c
LADRAT Mattéo <apokalyptdev@gmail.com>
2021-04-03 19:03:04 +0100

    -- Patch error -- Change color of few items

---
+++ front_end/app/src/main/java/com/example/nearyou/MyApplication.kt
@@ -0,0 +1,25 @@
+package com.example.nearyou
+
+import android.app.Application
+import android.content.res.Configuration
+import androidx.appcompat.app.AppCompatDelegate
+import com.example.nearyou.model.database.DatabaseHandler
+import java.util.*
+
+class MyApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        val currentTheme = DatabaseHandler(this).getConf("theme")
+        if (currentTheme == "light") {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        }
+
+        val conf: Configuration = resources.configuration
+        conf.setLocale(Locale(DatabaseHandler(this).getConf("language")))
+        resources.updateConfiguration(conf, resources.displayMetrics)
+    }
+}
